fc6f4bd980d0cd3769dd5c4b7d4c7d64f9158e80
Estifanos58 <estifkebe08@gmail.com>
2025-05-06 16:30:20 +0300

    update: home screen in progress

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,7 +1,8 @@
 import RideCard from '@/components/RideCard'
+import { icons, images } from '@/constants'
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
-import { FlatList, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const recentRides = [
@@ -105,10 +106,34 @@ const recentRides = [
 
 export default function Page() {
   const { user } = useUser()
+  const loading =  true;
 
   return (
     <SafeAreaView className='bg-general-500'>
-      <FlatList data={recentRides?.slice(0,5)} renderItem={({item}) => <RideCard ride={item}/>} keyExtractor={(item)=> item.ride_id}/>
+      <FlatList data={recentRides?.slice(0,5)} renderItem={({item}) => <RideCard ride={item}/>} keyExtractor={(item)=> item.ride_id} className='px-5' keyboardShouldPersistTaps="handled" contentContainerStyle={{
+        paddingBottom: 100, // Adjust this value as needed
+      }}
+      ListEmptyComponent={()=>(
+        <View className='flex flex-col items-center justify-center'>
+          {!loading ? 
+          <>
+          <Image source={images.noResult} className='w-40 h-40' alt='No recent rides found' resizeMode='contain'/>
+          <Text className='text-sm'>No recent rides found</Text> 
+          </>
+          : (
+            <ActivityIndicator size="small" color={"#000"}/>
+          )}
+        </View>
+      )}
+      ListHeaderComponent={()=> (
+        <View className='flex flex-row items-center justify-between my-5'>
+            <Text className='text-xl font-JakartaExtraBold'>Welcome {user?.firstName || (user?.emailAddresses[0].emailAddress)}👋</Text>
+            <TouchableOpacity>
+              <Image source={icons.out} className='w-4 h-4'/>
+            </TouchableOpacity>
+        </View>
+      )}
+      />
     </SafeAreaView>
   )
 }
